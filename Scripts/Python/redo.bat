b9e3d4f05ec1fa1186b33463a0d709433d3a7558
koenberton <koen.berton@skynet.be>
2021-09-20 17:17:33 +0200

    Add files via upload

---
+++ Scripts/Python/redo.bat
@@ -0,0 +1,21 @@
+python covid01.txt
+python covid02.txt
+python covid03.txt
+python covid04.txt
+python covid05.txt
+python covid06.txt
+python covid07.txt
+
+python abalone01.txt
+python abalone02.txt
+python abalone03.txt
+python abalone04.txt
+
+python dairy01.txt
+python dairy02.txt
+python dairy03.txt
+python dairy04.txt
+python dairy05.txt
+
+python kmeans.txt
+
